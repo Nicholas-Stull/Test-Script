2aedf67001f25237aa0eb94bc2b8b2617e57c5ad
Nicholas <Nicholasp.stull@gmail.com>
2022-11-05 22:12:31 -0400

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl -sL https://install.raspap.com | bash
+sudo apt install apt-transport-https lsb-release
+curl https://repo.jellyfin.org/debian/jellyfin_team.gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/jellyfin-archive-keyring.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/jellyfin-archive-keyring.gpg arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/debian $( lsb_release -c -s ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
+sudo apt update
+sudo apt install jellyfin
